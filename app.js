66457b16e9ebf8c52c2bda51deb5528ca6d2f291
Muhamad Nur Syami <nursami385@gmail.com>
2023-11-20 23:12:05 +0700

    berhasil setup project dan connect mongoDB atlas

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const db = require("./config/db");
+
+// check db connection
+db.then(() => {
+  console.log("berhasil connect ke MongoDB");
+}).catch(() => {
+  console.log("gagal connect ke MonggoDB");
+});
+// Midlleware
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// ROUTES
+
+// RUN SERVER
+app.listen(PORT, () => {
+  console.log(`Server Running on PORT ${PORT}`);
+});
